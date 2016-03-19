6c4d4e9fe00c4dcb8cb83dec83888bf359a56b17
Iván González <ivancito-02@live.com.mx>
2016-03-19 18:52:15 +0000

    Button layout, add symbol

--- views/buy_form.php
+++ views/buy_form.php
@@ -2,10 +2,10 @@
 <form action="buy.php" method="post">
     <fieldset>
         <div class="form-group">
-            <input class="form-control" name="symbol" placeholder="Symbol" type="text"/>
+            <input class="form-control" value="<?= $symbol_name ?>" name="symbol" placeholder="Symbol" type="text"/>
         </div>
         <div class="form-group">
-            <input class="form-control" name="shares" placeholder="Shares" type="text"/>
+            <input class="form-control" name="shares" placeholder="No. of Shares" type="text"/>
         </div>
         <div class="form-group">
             <button class="btn btn-default" type="submit">
