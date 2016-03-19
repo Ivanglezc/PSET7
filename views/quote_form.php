c9e090a07e07e3871b09e9da53d42891e7661dde
Iván González <ivancito-02@live.com.mx>
2016-03-19 19:33:02 +0000

    Color change

--- views/quote_form.php
+++ views/quote_form.php
@@ -1,7 +1,8 @@
+<body style="background-color:#bfbfbf">
 <form action="quote.php" method="post">
     <fieldset>
         <div class="form-group">
-            <input autocomplete="off" autofocus class="form-control" name="symbol" placeholder="symbol" type="text"/>
+            <input autocomplete="off" autofocus class="form-control" name="symbol" placeholder="Symbol" type="text"/>
         </div>
         <div class="form-group">
             <button class="btn btn-default" type="submit">
